97ae317d0be82dad0d00cdff9fff741f92bb7886
Lívia Vilhena <liviavilhena@outlook.com>
2024-12-16 15:36:19 -0300

    Add files via upload

---
+++ Jogo da velhaa/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace Jogo_da_velhaa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblpontos = new System.Windows.Forms.Label();
+            this.lbl0pontos = new System.Windows.Forms.Label();
+            this.lblXpontos = new System.Windows.Forms.Label();
+            this.btnclean = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btn);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(480, 310);
+            this.panel1.TabIndex = 0;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button8.Font = new System.Drawing.Font("Muli", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(309, 217);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(99, 80);
+            this.button8.TabIndex = 8;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button7.Font = new System.Drawing.Font("Muli", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(184, 217);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(99, 80);
+            this.button7.TabIndex = 7;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.Font = new System.Drawing.Font("Muli", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(53, 217);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(99, 80);
+            this.button6.TabIndex = 6;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.Font = new System.Drawing.Font("Muli", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(309, 121);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(99, 80);
+            this.button5.TabIndex = 5;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Font = new System.Drawing.Font("Muli", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(184, 121);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 80);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Font = new System.Drawing.Font("Muli", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(53, 121);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 80);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Muli", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(309, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 80);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Muli", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(184, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 80);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn
+            // 
+            this.btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn.Font = new System.Drawing.Font("Muli", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn.Location = new System.Drawing.Point(53, 23);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(99, 80);
+            this.btn.TabIndex = 0;
+            this.btn.UseVisualStyleBackColor = false;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.lblpontos);
+            this.panel2.Controls.Add(this.lbl0pontos);
+            this.panel2.Controls.Add(this.lblXpontos);
+            this.panel2.Location = new System.Drawing.Point(12, 379);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(480, 90);
+            this.panel2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(338, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Empate";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(196, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "O pontos";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(65, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "X pontos";
+            // 
+            // lblpontos
+            // 
+            this.lblpontos.AutoSize = true;
+            this.lblpontos.Font = new System.Drawing.Font("Muli", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpontos.Location = new System.Drawing.Point(369, 53);
+            this.lblpontos.Name = "lblpontos";
+            this.lblpontos.Size = new System.Drawing.Size(31, 34);
+            this.lblpontos.TabIndex = 2;
+            this.lblpontos.Text = "0";
+            // 
+            // lbl0pontos
+            // 
+            this.lbl0pontos.AutoSize = true;
+            this.lbl0pontos.Font = new System.Drawing.Font("Muli", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl0pontos.Location = new System.Drawing.Point(227, 53);
+            this.lbl0pontos.Name = "lbl0pontos";
+            this.lbl0pontos.Size = new System.Drawing.Size(31, 34);
+            this.lbl0pontos.TabIndex = 1;
+            this.lbl0pontos.Text = "0";
+            // 
+            // lblXpontos
+            // 
+            this.lblXpontos.AutoSize = true;
+            this.lblXpontos.Font = new System.Drawing.Font("Muli", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblXpontos.Location = new System.Drawing.Point(95, 53);
+            this.lblXpontos.Name = "lblXpontos";
+            this.lblXpontos.Size = new System.Drawing.Size(31, 34);
+            this.lblXpontos.TabIndex = 0;
+            this.lblXpontos.Text = "0";
+            // 
+            // btnclean
+            // 
+            this.btnclean.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclean.Location = new System.Drawing.Point(112, 328);
+            this.btnclean.Name = "btnclean";
+            this.btnclean.Size = new System.Drawing.Size(276, 45);
+            this.btnclean.TabIndex = 2;
+            this.btnclean.Text = "Limpar";
+            this.btnclean.UseVisualStyleBackColor = true;
+            this.btnclean.Click += new System.EventHandler(this.btnclean_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(504, 481);
+            this.Controls.Add(this.btnclean);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jogo da velha";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblpontos;
+        private System.Windows.Forms.Label lbl0pontos;
+        private System.Windows.Forms.Label lblXpontos;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnclean;
+    }
+}
+
---
+++ Jogo da velhaa/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jogo_da_velhaa
+{
+    public partial class Form1 : Form
+    {
+
+        int Xplayer = 0;
+        int Oplayer = 0;
+        int empatePontos = 0;
+        int rodadas = 0;
+
+        bool turno = true;
+        bool jogoFinal = false;
+        string[] texto = new string[9];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            int buttonIndex = btn.TabIndex;
+
+            if (btn.Text == "" && jogoFinal == false)
+            {
+                if (turno)
+                {
+                    btn.Text = "X";
+                    texto[buttonIndex] = btn.Text;
+                    rodadas++;
+                    turno = !turno;
+                    Checagem(1);
+                }
+                else
+                {
+                    btn.Text = "O";
+                    texto[buttonIndex] = btn.Text;
+                    rodadas++;
+                    turno = !turno;
+                    Checagem(2);
+                }
+            }  
+
+        }
+
+        void Vencedor(int PlayerQueGanhou)
+        {
+            jogoFinal = true;
+            
+            if (PlayerQueGanhou == 1)
+            {
+                Xplayer++;
+                lblXpontos.Text = Convert.ToString(Xplayer);
+                MessageBox.Show("Jogador X ganhou!");
+                turno = true;
+            }
+            else
+            {
+                Oplayer++;
+                lbl0pontos.Text = Convert.ToString(Oplayer);
+                MessageBox.Show("Jogador O ganhou!");
+                turno = false;
+            }
+        }
+
+
+        void Checagem(int Checagemplayer)
+        {
+            string suporte = " ";
+
+            if (Checagemplayer == 1)
+            {
+                suporte = "X";
+            }
+            else
+            {
+                suporte = "O";
+            }
+            
+            // Checagem horizontal
+            for (int horizontal = 0; horizontal < 8; horizontal += 3 )
+            {
+                if (suporte == texto[horizontal])
+                {
+                    if (texto[horizontal] == texto[horizontal + 1 ] && texto[horizontal] == texto[horizontal + 2])
+                    {
+                        Vencedor(Checagemplayer);
+                        return;
+                    }
+                }
+            }
+
+            // Checagem vertical
+            for (int vertical = 0; vertical < 3; vertical += 3)
+            {
+                if (suporte == texto[vertical])
+                {
+                    if (texto[vertical] == texto[vertical + 3] && texto[vertical] == texto[vertical + 6])
+                    {
+                        Vencedor(Checagemplayer);
+                        return;
+                    }
+                }
+            }
+
+            // Chegagem Diagonal
+
+            if (texto[0] == suporte)
+            {
+
+                if (texto[0] == texto[4] && texto[0] == texto[8])
+                {
+                    Vencedor(Checagemplayer);
+                    return;
+                }
+
+            }
+            if ( texto[2] == suporte)
+            {
+                if (texto[2] == texto[4] && texto[2] == texto[6])
+                {
+                    Vencedor(Checagemplayer);
+                    return;
+                }
+
+            }
+
+            if (rodadas == 9 && jogoFinal == false)
+            {
+                empatePontos++;
+                lblpontos.Text = Convert.ToString(empatePontos);
+                MessageBox.Show("Empate!");
+                jogoFinal = true;
+                return;
+            }
+
+
+
+
+
+        }
+
+        private void btnclean_Click(object sender, EventArgs e)
+        {
+            btn.Text = "";
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+
+
+            rodadas = 0;
+            jogoFinal = false;
+
+            for (int i = 0; i < 9; i++)
+            {
+
+                texto[i] = "";
+            }
+        }
+    }
+}
